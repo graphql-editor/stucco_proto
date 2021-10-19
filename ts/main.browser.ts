c8291173d2cd2769528218eef2ffe84ff6a21511
Kamil Matysiewicz <kamil@aexol.com>
2021-10-19 14:58:41 +0200

    Start supporting es6 modules properly

--- ts/main.browser.ts
+++ ts/main.browser.ts
@@ -1,5 +1,2 @@
-/* tslint:disable */
-/* eslint-disable */
-
-export * as messages from './web/messages/messages_pb';
-export * as driverService from './web/driver_service/service_grpc_web_pb';
+export * as messages from './web/messages/messages_pb.cjs';
+export * as driverService from './web/driver_service/service_grpc_web_pb.cjs';
--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,2 @@
-/* tslint:disable */
-/* eslint-disable */
-
-export * as messages from './node/messages/messages_pb';
-export * as driverService from './node/driver_service/service_grpc_pb';
+export * as messages from './node/messages/messages_pb.cjs';
+export * as driverService from './node/driver_service/service_grpc_pb.cjs';
